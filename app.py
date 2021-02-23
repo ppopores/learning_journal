97d0eb829af0402019097417c72a7d7f9928e8ca
ppopores <ppopores@gmail.com>
2021-02-22 19:14:39 -0800

    working app.py

--- app.py
+++ app.py
@@ -1,2 +1,30 @@
 from flask import (Flask, g, render_template, flash, redirect, url_for,
                    abort)
+from flask.ext.bcrypt import check_password_hash
+from flask.ext.login import (LoginManager, login_user, logout_user,
+                             login_required, current_user)
+
+import forms
+import models
+
+DEBUG = True
+PORT = 8000
+HOST = '0.0.0.0'
+
+app = Flask(__name__)
+app.secret_key = 'sadfjksdf9sadvcnasdfl0.14cv.asdf541asd.'
+
+login_manager - LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+
+@login_manager.user_loader
+def load_user(userid):
+    try:
+        return models.User.get(models.User.id == userid)
+    except models.DoesNotExist:
+        return None
+
+
+@app.before_request
